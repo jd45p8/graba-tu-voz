32bf404c4448040093d1ba48c2dd9c1ee99c9d06
José D. Polo Bolaño <jd45p8@gmail.com>
2020-03-25 17:04:59 -0500

    Se terminó la página inicial.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,75 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app color="white">
+      <div class="d-flex align-center">
+        <v-img
+          alt="Graba tu voz - logo"
+          class="shrink mr-2"
+          contain
+          src="./assets/logo-colorfull.png"
+          transition="scale-transition"
+          width="30"
+        />
+        <h1 class="dark-text display-1 hidden-sm-and-down">raba tu voz</h1>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
+        <span class="mr-2">Iniciar sesión</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-content class="my-4">
+      <router-view></router-view>
+    </v-content>
+    <v-footer color="white" class="justify-center">
+      <p>Desarrollado por José Polo <a href="https://www.github.com/jd45p8">@jd45p8</a></p>
+    </v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style lang="scss">
+$color-blue-dark: #4f81c7ff;
+$color-blue-light: #00a8b5ff;
+$color-green-light: #2bb883ff;
+$color-dark: #333333ff;
+
+.dark-text {
+  color: $color-dark;
 }
 
-#nav {
-  padding: 30px;
+.blue-dark-text {
+  color: $color-blue-dark;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.blue-light-text {
+  color: $color-blue-light;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.green-light-text {
+  color: $color-green-light;
+}
+
+.dark {
+  background-color: $color-dark !important;
+}
+
+.blue-dark {
+  background-color: $color-blue-dark !important;
+}
+
+.blue-light {
+  background-color: $color-blue-light !important;
+}
+
+.green-light {
+  background-color: $color-green-light !important;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,69 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <v-img
+      alt="Graba tu voz logo"
+      src="../assets/logo-colorfull.png"
+      class="align-center d-flex"
+      contain
+      max-height="200"
+      height="50%"
+    />
+    <v-col>
+      <!-- Fila de bienvenida -->
+      <v-row justify="center" class="mx-1">
+        <v-col lg="6" md="6" sm="8">
+          <v-row justify="center">
+            <h1 class="display-2 text-center">
+              ¡Bienvenido a
+              <b class="blue-dark-text">Graba </b>
+              <b class="blue-light-text">tu </b>
+              <b class="green-light-text">voz</b>!
+            </h1>
+          </v-row>
+          <v-row justify="center" class="mt-6">
+            <p class="font-weight-light title text-center text-md-left">
+              Un proyecto para la recolección de voces en
+              <b>español</b> para el entrenamiento de un modelo de reconocimiento de voz que será implementado por estudiantes de ingeniería de sistemas en su proyecto final.
+            </p>
+          </v-row>
+        </v-col>
+      </v-row>
+      <!-- Fila con el porqué deben apoyarnos -->
+      <v-row justify="center" class="mx-md-4 mx-lg-8 mx-xl-12">
+        <v-col cols="12" sm="6" md="4" class="flex-wrap">
+          <v-card class="blue-dark pa-3 white--text d-flex flex-column" height="100%">
+            <div class="my-auto">
+              <h1 class="display-2 text-center">Regístrate</h1>
+              <h2 class="font-weight-light subtitle-1 mt-2">Para identificar una persona por su voz, se necesita separar las voces de distintos individuos, por lo que necesitamos que se registre para participar.</h2>
+            </div>
+          </v-card>
+        </v-col>
+        <v-col cols="12" sm="6" md="4" class="flex-wrap">
+          <v-card class="blue-light pa-3 white--text d-flex flex-column" height="100%">
+            <div class="my-auto">
+              <h1 class="display-2 text-center">Graba</h1>
+              <h2 class="font-weight-light subtitle-1 mt-2">Los conjuntos de datos de voz en español no son abundantes, y es aún más dificil obtenerlos separados por individuos, por eso te invitamos a grabar tu voz para apoyar nuestro proyecto.</h2>
+            </div>  
+          </v-card>
+        </v-col>
+        <v-col cols="12" sm="6" md="4" class="flex-wrap">
+          <v-card class="green-light pa-3 white--text d-flex flex-column" height="100%">
+            <div class="my-auto">
+              <h1 class="display-2 text-center">Apoya</h1>
+              <h2 class="font-weight-light subtitle-1 mt-2">La realización de proyectos como este, ayudan al desarrollo de soluciones para público hispano, que permitan brindar una mejor experiencia a todos estos usuarios.</h2>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-col>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  name: "Home"
 };
 </script>
+
