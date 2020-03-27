8e8cf8c8a2116f3d31b1a21011b6d15b4fe98336
José D. Polo Bolaño <jd45p8@gmail.com>
2020-03-27 03:55:01 -0500

    La grabación no funciona por alguna razón.

--- src/components/RecordModal.vue
+++ src/components/RecordModal.vue
@@ -5,8 +5,18 @@
         <v-card-title class="headline justify-center">Grabar</v-card-title>
         <v-card-text class="subtitle-1">Debes grabar tu voz diciendo: "{{text}}"</v-card-text>
 
-        <v-btn color="blue-dark" dark fab depressed x-large class="mx-auto" @click="startRecording">
-          <v-icon>mdi-microphone</v-icon>
+        <v-btn
+          color="blue-dark"
+          v-if="!finishedRecording"
+          dark
+          fab
+          depressed
+          x-large
+          class="mx-auto"
+          @click="startRecording"
+        >
+          <v-icon v-if="!recording">mdi-microphone</v-icon>
+          <v-icon v-if="recording">mdi-stop</v-icon>
         </v-btn>
 
         <v-card-actions>
@@ -24,7 +34,9 @@ export default {
   name: "RecordModal",
   data: function() {
     return {
-      status: ""
+      recording: false,
+      shouldStop: false,
+      finishedRecording: false
     };
   },
   methods: {
@@ -32,34 +44,58 @@ export default {
       this.$emit("closeModal");
     },
     startRecording: async function() {
-      var handleSuccess = stream => {
-        var context = new AudioContext();
-        var input = context.createMediaStreamSource(stream);
-        var processor = context.createScriptProcessor(1024, 1, 1);
+      const self = this;
 
-        input.connect(processor);
-        processor.connect(context.destination);
+      var handleSuccess = function(stream) {
+        const options = { mimeType: "video/webm;codecs=vp9" };
+        const chunks = [];
+        const recorder = new MediaRecorder(stream, options);
 
-        processor.onaudioprocess = e => {
-          console.log(e.inputBuffer);
+        self.recording = true;
+        self.shouldStop = false;
+
+        recorder.ondataavailable = function(e) {
+          if (e.data.size > 0) {
+            chunks.push(e.data);
+          }
+
+          console.log(e.data);
+
+          if (self.shouldStop && self.recording) {
+            recorder.stop();
+            self.recording = false;
+            self.finishedRecording = true;
+            self.shouldStop = false;
+          }
         };
+
+        recorder.onstop = function(e) {
+          console.log("Finished");
+        };
+
+        recorder.start();
       };
 
-      var permision = await this.checkPermision()
+      if (!this.recording) {
+        var permision = await this.checkPermision();
 
-      if (permision != "denied") {
-        navigator.mediaDevices
-          .getUserMedia({ audio: true, video: false })
-          .then(handleSuccess);
-      } else {
-        alert("¡No ha sido posible obtener acceso al micrófono!");
+        if (permision != "denied") {
+          navigator.mediaDevices
+            .getUserMedia({ audio: true, video: false })
+            .then(handleSuccess);
+        } else {
+          alert("¡No ha sido posible obtener acceso al micrófono!");
+        }
+      } else if (!this.shouldStop) {
+        this.shouldStop = true;
       }
+      console.log(`${this.recording}  ${this.shouldStop}`)
     },
     checkPermision: function() {
       const self = this;
       return new Promise((resolve, reject) => {
         navigator.permissions.query({ name: "microphone" }).then(result => {
-          resolve(result.state)
+          resolve(result.state);
         });
       });
     }
