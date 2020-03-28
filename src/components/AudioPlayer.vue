5ac13d1088f9d4077d09007392130e5c3719c1c5
José D. Polo Bolaño <jd45p8@gmail.com>
2020-03-28 10:46:55 -0500

    Se añadió un componente personalizado para la repoducción de audio.

---
+++ src/components/AudioPlayer.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="audioplayer">
+    <audio :id="id" :src="src" controls preload="auto" hidden></audio>
+    <v-slider
+      v-model="sliderPosition"
+      hide-details
+      class="align-center blue-dark-text"
+      track-color="gray"
+      color="blue-dark"
+      :max="maxSlider"
+      @change="updatePlayerCurrentTime"
+      @start="player.pause()"
+    >
+      <template v-slot:prepend>
+        <v-btn @click="togglePlay" color="grey darken-1" fab small text>
+          <v-icon>{{isPlaying ? 'mdi-pause' : 'mdi-play'}}</v-icon>
+        </v-btn>
+      </template>
+      <template v-slot:append>
+        <h1 class="dark-text subtitle-2 font-weight-regular" style="width: max-content">{{textTime}}</h1>
+      </template>
+    </v-slider>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AudioPlayer",
+  data: function() {
+    return {
+      isPlaying: false,
+      currentTime: 0,
+      duration: 0,
+      textTime: "0:00",
+      sliderPosition: 0,
+      maxSlider: 1000
+    };
+  },
+  props: {
+    id: {
+      required: true,
+      type: String
+    },
+    src: {
+      required: true,
+      type: String
+    }
+  },
+  methods: {
+    togglePlay: function() {
+      if (this.isPlaying) {
+        this.player.pause();
+      } else {
+        this.player.play();
+      }
+    },
+    updatePlayerCurrentTime: function() {
+      this.player.currentTime =
+        (this.sliderPosition / this.maxSlider) * this.duration;
+    },
+    updateTextTime: function() {
+      let currentMin = Math.floor(this.currentTime / 60);
+      let currentSeg = Math.floor(this.currentTime % 60);
+      let current = `${currentMin}:${
+        currentSeg > 9 ? currentSeg : `0${currentSeg}`
+      }`;
+
+      let maxMin = Math.floor(this.duration / 60);
+      let maxSeg = Math.floor(this.duration % 60);
+      let max = `${maxMin}:${maxSeg > 9 ? maxSeg : `0${maxSeg}`}`;
+
+      if (this.duration > 0) {
+        this.sliderPosition =
+          this.maxSlider * (this.currentTime / this.duration);
+        this.textTime = `${current} / ${max}`;
+      }else{
+        this.textTime = "0:00 / 0:00"
+      }
+    }
+  },
+  computed: {
+    player: function() {
+      return document.getElementById(this.id);
+    }
+  },
+  watch: {
+    currentTime: function() {
+      this.updateTextTime();
+    },
+    duration: function() {
+      this.updateTextTime();
+    }
+  },
+  mounted: function() {
+    this.player.volume = 1;
+    this.duration = this.player.duration;
+
+    this.player.addEventListener("play", e => {
+      this.isPlaying = true;
+    });
+
+    this.player.addEventListener("ended", e => {
+      this.isPlaying = false;
+    });
+
+    this.player.addEventListener("pause", e => {
+      this.isPlaying = false;
+    });
+
+    this.player.addEventListener("timeupdate", e => {
+      this.currentTime = this.player.currentTime;
+    });
+
+    this.player.addEventListener("durationchange", e => {
+      this.duration = this.player.duration;
+    });
+  }
+};
+</script>
--- src/views/UserDashBoard.vue
+++ src/views/UserDashBoard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="userdashboard">
-    <record-modal :open="showRecordModal" :text="textToRecord" @closeModal="closeRecordModal"/>
+    <record-modal :open="showRecordModal" :text="textToRecord" @closeModal="closeRecordModal" />
     <v-col class="mt-n5">
       <v-row>
         <template v-for="i in 10">
@@ -9,30 +9,27 @@
               <v-expansion-panel>
                 <v-expansion-panel-header>
                   <v-row align="center" class="dark-text">
-                    <v-col cols="10">
-                      Frase {{i-1}}
-                    </v-col>
-                    <v-col cols="2" class="blue-dark-text">
-                      1/5
-                    </v-col>
+                    <v-col cols="10">Frase {{i-1}}</v-col>
+                    <v-col cols="2" class="blue-dark-text">1/5</v-col>
                   </v-row>
                 </v-expansion-panel-header>
                 <v-expansion-panel-content>
-                  <v-card v-for="i in 4" :key="i" outlined :class="{'my-2': i > 1}">
+                  <v-card v-for="j in 4" :key="j" outlined :class="{'my-2': j > 1}">
                     <v-list-item>
                       <v-list-item-content>
                         <div class="dark-text subtitle-2 font-weight-regular">Grabación {{i}}</div>
                       </v-list-item-content>
-                      <v-btn color="grey darken-1" fab small text>
-                        <v-icon>mdi-delete</v-icon>
-                      </v-btn>
-                      <v-btn color="grey darken-1" fab small text>
-                        <v-icon>mdi-play</v-icon>
-                      </v-btn>
+                      <audio-player :id="`player-${i}-${j}`" src=""></audio-player>
                     </v-list-item>
                   </v-card>
                   <v-row justify="center">
-                    <v-btn @click="openRecordModal((i-1).toString())" color="blue-dark" dark fab depressed>
+                    <v-btn
+                      @click="openRecordModal((i-1).toString())"
+                      color="blue-dark"
+                      dark
+                      fab
+                      depressed
+                    >
                       <v-icon>mdi-plus</v-icon>
                     </v-btn>
                   </v-row>
@@ -47,28 +44,30 @@
 </template>
 
 <script>
-import RecordModal from '../components/RecordModal.vue'
+import RecordModal from "../components/RecordModal.vue";
+import AudioPlayer from "../components/AudioPlayer.vue";
 
 export default {
   nane: "UserDashBoard",
   data: function() {
     return {
       showRecordModal: false,
-      textToRecord: ''
-    }
+      textToRecord: ""
+    };
   },
   methods: {
-    openRecordModal: function (text) {
-      this.textToRecord = text
-      this.showRecordModal = true
+    openRecordModal: function(text) {
+      this.textToRecord = text;
+      this.showRecordModal = true;
     },
-    closeRecordModal: function () {
-      this.showRecordModal = false
-      this.textToRecord = ''
+    closeRecordModal: function() {
+      this.showRecordModal = false;
+      this.textToRecord = "";
     }
   },
   components: {
-    RecordModal
+    RecordModal, 
+    AudioPlayer
   }
 };
 </script>
