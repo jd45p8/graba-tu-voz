104a5714019d4035bd3dfbff8f1be98b88121770
José D. Polo Bolaño <jd45p8@gmail.com>
2020-04-04 23:29:05 -0500

    Se permite cerrar sesión.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
+import { notificationBus } from "../main"
 
 Vue.use(VueRouter);
 
@@ -8,7 +9,7 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: () => import('../views/Home.vue'),
+    component: () => import("../views/Home.vue"),
     meta: {
       shownName: "Inicio"
     }
@@ -16,33 +17,64 @@ const routes = [
   {
     path: "/join",
     name: "Join",
-    component: () => import('../views/Join.vue'),
+    component: () => import("../views/Join.vue"),
     meta: {
       guest: true,
       shownName: "Unirse"
     }
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: () => import('../views/Login.vue'),
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/Login.vue"),
     meta: {
       guest: true,
       shownName: "Inciar sesión"
     }
   },
   {
-    path: '/userdashboard',
-    name: 'UserDashBoard',
-    component: () => import('../views/UserDashBoard.vue'),
+    path: "/userdashboard",
+    name: "UserDashBoard",
+    component: () => import("../views/UserDashBoard.vue"),
     meta: {
       requiresAuth: true,
       shownName: "Grabaciones"
     }
+  }, {
+    path: "/logout",
+    mane: "Logout",
+    meta: {
+      requiresAuth: true,
+      shownName: "Cerrar sesión"
+    },
+    beforeEnter: async function (to, from, next) {
+      const axios = require("axios");
+      try {
+        let response = await axios({
+          method: "post", url: `${window["URL_API"]}/logout`,
+          headers: {
+            Authorization: `Bearer ${localStorage.token}`
+          }
+        });
+        notificationBus.$emit("SUCCESS", response.data.message);
+      } catch (error) {
+        if (error.response.status >= 500) {
+          notificationBus.$emit("ERROR", error.response.data.message);
+        } else {
+          notificationBus.$emit("WARNING", error.response.data.message);
+        }
+      }
+
+      localStorage.removeItem("token");
+      localStorage.removeItem("email");
+      next({
+        name: "Login"
+      });
+    }
   },
   {
-    path: '*',
-    redirect: {name: 'Home'}
+    path: "*",
+    redirect: {name:'Home'}
   }
 ];
 
@@ -56,15 +88,15 @@ router.beforeEach((to, from, next) => {
   if (to.matched.some(r => r.meta.requiresAuth)) {
     if (!localStorage.token) {
       return next({
-        name: 'Login'
+        name: "Login"
       });
     }
   }
 
-  if (to.matched.some( r => r.meta.guest)) {
-    if (localStorage.token){
+  if (to.matched.some(r => r.meta.guest)) {
+    if (localStorage.token) {
       return next({
-        name: 'UserDashBoard'
+        name: "UserDashBoard"
       });
     }
   }
